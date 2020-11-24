130fca005deaa3d535e160fc72b43349fa12831d
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-24 14:17:26 +0100

    CategoriesCard imported into home.js component

--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CategoriesCard from '../components/categories-card';
 import Hero from '../components/hero';
 
 function Home() {
   return (
     <>
       <Hero />
+      <CategoriesCard />
     </>
   );
 }
