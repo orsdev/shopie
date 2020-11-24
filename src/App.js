e4f821463164e1df2da5e33ad322432498dad5bb
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-24 03:17:45 +0100

    component imported into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { Suspense } from 'react';
+import LoadingScreen from './components/loading-screen';
+import Header from './components/header';
+import Router from './router/router';
 import './assets/css/style.css';
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Suspense fallback={<LoadingScreen />}>
+        <Header />
+        <Router />
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
