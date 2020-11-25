148bd9af534021c4531a6e09605231f7ce1d1dfb
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-25 15:32:28 +0100

    cart items rendered dynamically to cartMenu component

--- client/src/components/cartMenu.js
+++ client/src/components/cartMenu.js
@@ -4,6 +4,7 @@ import toggleCart from '../redux/actions/toggleCart.action';
 
 const CartMenu = () => {
   const { displayCart } = useSelector((state) => state.toggleCart);
+  const { cartItems } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   function hideCart() {
@@ -17,94 +18,48 @@ const CartMenu = () => {
         <div className="cart-menu-container-show">
           <div className="cart-menu-container-show-header">
             <h2>Cart</h2>
-            <i onClick={hideCart} className="fa fa-times" aria-hidden="true" />
+            <i
+              onClick={hideCart}
+              className="fa fa-times"
+              aria-hidden="true"
+            />
           </div>
           <div className="cart-menu-container-show-body mt-4">
-            <div className="cart-menu-card">
-              <img
-                src="https://ae01.alicdn.com/kf/HTB13yaiacvrK1Rjy0Feq6ATmVXaO/Retro-Genuine-Leather-Men-s-Backpack-Large-Capacity-Laptop-Bag-School-Backpack-Male-Shoulder-Bags-Brown.jpg"
-                height="auto"
-                alt="productImage"
-              />
-              <div className="cart-menu-card-details ml-3">
-                <h3 className="cart-menu-card-name">
-                  Go backpack with brown straps and buckles.
-              </h3>
-                <p className="cart-menu-card-price-and-quantity">
-                  25 x 3
-              </p>
-              </div>
-            </div>
-            <div className="cart-menu-card">
-              <img
-                src="https://ae01.alicdn.com/kf/HTB13yaiacvrK1Rjy0Feq6ATmVXaO/Retro-Genuine-Leather-Men-s-Backpack-Large-Capacity-Laptop-Bag-School-Backpack-Male-Shoulder-Bags-Brown.jpg"
-                height="auto"
-                alt="productImage"
-              />
-              <div className="cart-menu-card-details ml-3">
-                <h3 className="cart-menu-card-name">
-                  Go backpack with brown straps and buckles.
-              </h3>
-                <p className="cart-menu-card-price-and-quantity">
-                  25 x 3
-              </p>
-              </div>
-            </div>
-            <div className="cart-menu-card">
-              <img
-                src="https://ae01.alicdn.com/kf/HTB13yaiacvrK1Rjy0Feq6ATmVXaO/Retro-Genuine-Leather-Men-s-Backpack-Large-Capacity-Laptop-Bag-School-Backpack-Male-Shoulder-Bags-Brown.jpg"
-                height="auto"
-                alt="productImage"
-              />
-              <div className="cart-menu-card-details ml-3">
-                <h3 className="cart-menu-card-name">
-                  Go backpack with brown straps and buckles.
-              </h3>
-                <p className="cart-menu-card-price-and-quantity">
-                  25 x 3
-              </p>
-              </div>
-            </div>
-            <div className="cart-menu-card">
-              <img
-                src="https://ae01.alicdn.com/kf/HTB13yaiacvrK1Rjy0Feq6ATmVXaO/Retro-Genuine-Leather-Men-s-Backpack-Large-Capacity-Laptop-Bag-School-Backpack-Male-Shoulder-Bags-Brown.jpg"
-                height="auto"
-                alt="productImage"
-              />
-              <div className="cart-menu-card-details ml-3">
-                <h3 className="cart-menu-card-name">
-                  Go backpack with brown straps and buckles.
-              </h3>
-                <p className="cart-menu-card-price-and-quantity">
-                  25 x 3
-              </p>
-              </div>
-            </div>
-            <div className="cart-menu-card">
-              <img
-                src="https://ae01.alicdn.com/kf/HTB13yaiacvrK1Rjy0Feq6ATmVXaO/Retro-Genuine-Leather-Men-s-Backpack-Large-Capacity-Laptop-Bag-School-Backpack-Male-Shoulder-Bags-Brown.jpg"
-                height="auto"
-                alt="productImage"
-              />
-              <div className="cart-menu-card-details ml-3">
-                <h3 className="cart-menu-card-name">
-                  Go backpack with brown straps and buckles.
-              </h3>
-                <p className="cart-menu-card-price-and-quantity">
-                  25 x 3
-              </p>
-              </div>
-            </div>
+            {cartItems.length ? (
+              cartItems.map((item) => {
+                return (
+                  <div className="cart-menu-card" key={item.key + item.title}>
+                    <img
+                      src={item.image}
+                      height="auto"
+                      alt={item.title}
+                    />
+                    <div className="cart-menu-card-details ml-3">
+                      <h3 className="cart-menu-card-name">
+                        {item.title}
+                      </h3>
+                      <p className="cart-menu-card-price-and-quantity">
+                        ${item.price} x {item.quantityInCart}
+                      </p>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+                <h2 className="w-100 text-center py-5">Cart is empty.</h2>
+              )}
           </div>
-          <button
-            className="d-block my-5 w-75 mx-auto btn-dark btn-lg btn rounded-0 font-weight-bold py-3"
-            type="button">
-            Checkout
-        </button>
+          {cartItems.length ? (
+            <button
+              className="d-block my-5 w-75 mx-auto btn-dark btn-lg btn rounded-0 font-weight-bold py-3"
+              type="button">
+              Checkout
+            </button>
+          ) : null}
         </div>
       </div>
     </aside>
-  )
+  );
 };
 
 export default CartMenu;
