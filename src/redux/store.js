849e96e89a2929bc5bffe6bc9105af78cee93f73
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-25 10:38:35 +0100

    unused files removed from app

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
-
-// Note: this API requires redux@>=3.1.0
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
