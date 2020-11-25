10db70180357e7e3e1266efcf3349224f8903df8
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-25 05:43:17 +0100

    shop component imported into router.js

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,14 @@ import React, { lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 const HomePage = lazy(() => import('../views/home'));
+const ShopPage = lazy(() => import('../views/shop'));
 
 function Router() {
   return (
     <>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </>
   );
