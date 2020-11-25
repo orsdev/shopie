928e71745b7be2fd16c2d0ac0672f4cabc8cfb57
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-25 12:53:00 +0100

    productReducer combined in rootReducer.js

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import categoriesReducer from './reducers/categories.reducer';
+import productReducer from './reducers/product.reducer';
 import toggleCartReducer from './reducers/toggleCart.reducer';
 import errorReducer from './reducers/error.reducer';
 
 const rootReducer = combineReducers({
   error: errorReducer,
   categories: categoriesReducer,
+  products: productReducer,
   toggleCart: toggleCartReducer
 });
 
