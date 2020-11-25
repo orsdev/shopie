1915da165c587de975591875966624d6a7f2a6ea
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-25 15:33:29 +0100

    cart reducer combined in rootReducer.js

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import categoriesReducer from './reducers/categories.reducer';
 import productReducer from './reducers/product.reducer';
 import toggleCartReducer from './reducers/toggleCart.reducer';
+import Cart from './reducers/cart.reducer';
 import errorReducer from './reducers/error.reducer';
 
 const rootReducer = combineReducers({
   error: errorReducer,
   categories: categoriesReducer,
   products: productReducer,
+  cart: Cart,
   toggleCart: toggleCartReducer
 });
 
