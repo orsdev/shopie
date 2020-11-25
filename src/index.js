713180abb3d8f852ef6fcdd47efebac676ce1c07
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-25 09:53:44 +0100

    redux store added to Provider wrapper in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 import BackToTop from './utils/backToTop';
 import './index.css';
 
 const app = (
-  <BrowserRouter>
-    <BackToTop />
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <BackToTop />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
