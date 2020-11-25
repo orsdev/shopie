6a3e60ff0efdc2b1cc44c1cfd75d4aa056870182
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-26 00:08:36 +0100

    auth component imported into router.js

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 
 const HomePage = lazy(() => import('../views/home'));
 const ShopPage = lazy(() => import('../views/shop'));
+const CheckoutPage = lazy(() => import('../views/checkout'));
+const AuthPage = lazy(() => import('../views/auth'));
 
 function Router() {
   return (
@@ -10,6 +12,8 @@ function Router() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route exact path="/auth" component={AuthPage} />
       </Switch>
     </>
   );
