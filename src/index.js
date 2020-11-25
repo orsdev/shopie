33b5c86cc372084a5f15838edd73c6cf1362ee95
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-25 05:17:38 +0100

    backToTop component rendered in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import BackToTop from './utils/backToTop';
 import './index.css';
 
 const app = (
   <BrowserRouter>
+    <BackToTop />
     <React.StrictMode>
       <App />
     </React.StrictMode>
