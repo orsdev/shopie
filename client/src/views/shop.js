ed054cc4a99a1543feaca72bd33b536a5c9c7709
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-25 12:52:28 +0100

    products fetched from database

--- client/src/views/shop.js
+++ client/src/views/shop.js
@@ -1,62 +1,40 @@
-import React from 'react';
-import Black from '../assets/images/black-widow.png';
-import Blue from '../assets/images/blue-gown.png';
-import Purse from '../assets/images/blue-purse.png';
-import Bag from '../assets/images/leather-brown.png';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import getProducts from '../redux/actions/product.action';
 
 function Shop() {
+  /*
+ destructure & rename allProducts to products
+ */
+  const { allProducts: products } = useSelector(
+    (state) => state.products,
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <section className="shop container-fluid px-4">
       <h2 className="shop--header display-3 text-center">Shop</h2>
       <div className="product--grid text-center">
-        <div className="product--grid-item">
-          <img src={Black} alt="" />
-          <p className="product--name">Fila</p>
-          <p className="product--price">$120</p>
-          <p className="product--description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dolorum, ea.
-          </p>
-          <a href="/shop/ssdfdfdfj" className="product--cart-link">
-            ADD TO CART
-          </a>
-        </div>
-        <div className="product--grid-item">
-          <img src={Blue} alt="" />
-          <p className="product--name">Fila</p>
-          <p className="product--price">$120</p>
-          <p className="product--description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dolorum, ea.
-          </p>
-          <a href="/shop/ssdfdfdfj" className="product--cart-link">
-            ADD TO CART
-          </a>
-        </div>
-        <div className="product--grid-item">
-          <img src={Purse} alt="" />
-          <p className="product--name">Fila</p>
-          <p className="product--price">$120</p>
-          <p className="product--description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dolorum, ea.
-          </p>
-          <a href="/shop/ssdfdfdfj" className="product--cart-link">
-            ADD TO CART
-          </a>
-        </div>
-        <div className="product--grid-item">
-          <img src={Bag} alt="" />
-          <p className="product--name">Fila</p>
-          <p className="product--price">$120</p>
-          <p className="product--description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dolorum, ea.
-          </p>
-          <a href="/shop/ssdfdfdfj" className="product--cart-link">
-            ADD TO CART
-          </a>
-        </div>
+        {
+          products && products.map((prod) => {
+            return (
+              <div className="product--grid-item" key={prod.id + prod.title}>
+                <img src={prod.image} alt={prod.title} />
+                <p className="product--name">{prod.title.substring(0, 40) + '..'}</p>
+                <p className="product--price">${prod.price}</p>
+                <p className="product--description">{prod.description.substring(0, 80) + '..'}</p>
+                <button type="button" className="btn product--cart-btn">
+                  ADD TO CART
+                  </button>
+              </div>
+            )
+          })
+        }
       </div>
     </section>
   );
