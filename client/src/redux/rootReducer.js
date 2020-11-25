5ec0e138ab45196d09033b7095a8d69fd60d64e8
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-11-25 12:02:02 +0100

    toggleCart reducer combined in rootReducer

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import categoriesReducer from './reducers/categories.reducer';
+import toggleCartReducer from './reducers/toggleCart.reducer';
 import errorReducer from './reducers/error.reducer';
 
 const rootReducer = combineReducers({
   error: errorReducer,
   categories: categoriesReducer,
+  toggleCart: toggleCartReducer
 });
 
 export default rootReducer;
